0c432b17315755afa8d51f7328ddd2d1971e86fa
cbm888 <cbmarch88@gmail.com>
2021-12-07 21:33:10 -0600

    RPG Inventory

---
+++ RPGInventory.Tests/ContainerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RPGInventory.Items.Containers;
+using RPGInventory.Items.Potions;
+
+namespace RPGInventory.Tests
+{
+
+    [TestFixture]
+    public class ContainerTests
+    {
+        [Test]
+
+        public void CanAddItemToBackpack()
+        {
+            Backpack b = new Backpack();
+            HealthPotion p = new HealthPotion();
+
+            bool actual = b.AddItem(p);
+            Assert.AreEqual(true, actual);
+        }
+    }
+}
---
+++ RPGInventory/Items/Containers/Container.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGInventory.Items.Containers
+{
+   public abstract class Container : Item
+    {
+        // capacity - available to container, and it's children
+        protected int _capacity;
+        protected Item[] _items;
+        protected int _currentIndex;
+
+        public Container(int capacity)
+        {
+            _capacity = capacity;
+            _items = new Item[capacity];
+            _currentIndex = 0;
+        }
+
+        public bool AddItem(Item itemToAdd)
+        {
+            if (_capacity == _currentIndex)
+            {
+                return false;
+            }
+            else
+            {
+                _items[_currentIndex] = itemToAdd;
+                _currentIndex++;
+                return true;
+            }
+        }
+    }
+}
